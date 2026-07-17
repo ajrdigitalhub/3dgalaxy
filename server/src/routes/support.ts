f92214a34db3ff71aa1b4b837c6a705bf61f3db3
unknown <ajrgroupconnect@gmail.com>
2026-07-17 23:46:06 +0530

    updated server delete

--- server/src/routes/support.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { generateSupportMessage } from '../controllers/support';
-
-const router = Router();
-
-router.post('/generate', generateSupportMessage);
-
-export default router;
